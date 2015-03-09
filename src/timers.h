7de7824ffb45d0729b08dfe71d2507ced207021b
Marcel Groothuis <margro.xbmc@gmail.com>
2015-03-09 19:42:38 +0100

    License: correct the FSF address

--- src/timers.h
+++ src/timers.h
@@ -16,7 +16,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with XBMC; see the file COPYING.  If not, write to
  *  the Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
- *  MA 02110-1301  USA
+ *  MA 02110-1335  USA
  *  http://www.gnu.org/copyleft/gpl.html
  *
  */
